881e7c65b430496e5d8f5e6ca936e90f9fccfd3d
Kosty <doro.kon@mail.ru>
2022-11-30 22:33:19 +0300

    Add products

---
+++ js/supply.js
@@ -0,0 +1,17 @@
+import { create } from "./script.js";
+
+const product = {
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, sed.",
+    img: "/img/supply.png",
+    title: "Title",
+    price: 100,
+    about: "#",
+};
+const productContainer = document.querySelector(".product__container");
+
+let processors = [];
+for (let i = 0; i < 20; i++){
+    processors.push(product);
+}
+
+create(processors, productContainer);
